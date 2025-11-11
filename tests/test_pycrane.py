ab2dfeb2196526c6176f5c9351d75496c8040aa7
jakea10 <jakeatencio1@gmail.com>
2025-11-11 12:49:47 -0700

    add test for --version

--- tests/test_pycrane.py
+++ tests/test_pycrane.py
@@ -1,1 +1,13 @@
-# TODO: write basic test suites
+# tests/test_pycrane.py
+
+import pytest
+from typer.testing import CliRunner
+from pycrane import ___app_name__, __version__, cli
+
+
+runner = CliRunner()
+
+def test_version():
+    result = runner.invoke(cli.app, ["--version"])
+    assert result.exit_code == 0
+    assert f"{___app_name__} v{__version__}\n" in result.stdout
